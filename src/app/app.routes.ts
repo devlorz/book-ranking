c4a18100091ab432828c0b121545371b3cccd019
Nutti Saelor <nutti.sae@truedigital.com>
2024-03-05 23:19:27 +0700

    feat: add 404 component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { DetailComponent } from './components/detail/detail.component';
+import { PageNotFoundComponent } from './components/404/404.component';
 
-export const routes: Routes = [{ path: 'detail', component: DetailComponent }];
+export const routes: Routes = [
+  { path: 'detail', component: DetailComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
